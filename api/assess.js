c6a81d6ed9e723ed41b2457a28ea45e6b2b4938c
Ryan Kmetz <rmkenv@gmail.com>
2026-05-04 13:06:37 -0400

    Add files via upload

--- api/assess.js
+++ api/assess.js
@@ -1,6 +1,6 @@
-import Anthropic from "@anthropic-ai/sdk";
+const Anthropic = require("@anthropic-ai/sdk");
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
